9ba61700fae54e06ee8131551f7115b68828d939
amangrobo <amangrobo@gmail.com>
2019-01-05 20:11:32 +0530

    added feed item and timetable item

---
+++ app/src/main/java/com/grobo/notifications/models/FeedItem.java
@@ -0,0 +1,9 @@
+package com.grobo.notifications.models;
+
+public class FeedItem {
+
+    private int imageResourceId = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
+    private String eventName;
+    private String
+}
--- app/src/main/java/com/grobo/notifications/models/TimetableItem.java
+++ app/src/main/java/com/grobo/notifications/models/TimetableItem.java
@@ -1,4 +1,30 @@
 package com.grobo.notifications.models;
 
 public class TimetableItem {
+
+    private String time;
+    private String subject;
+
+    public TimetableItem(){}
+
+    public TimetableItem(String time, String subject){
+        this.time = time;
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
